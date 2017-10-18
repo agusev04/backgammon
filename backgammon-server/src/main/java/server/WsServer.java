9dc885f85697cf813f16ee579d1080c9bb3f7e23
IvchenkoAlexandr <aleksandr.ivchenko19@gmail.com>
2017-10-19 00:24:23 +0700

    Deleted tests and added hub for clients (hashmap of clients)

--- backgammon-server/src/main/java/server/WsServer.java
+++ backgammon-server/src/main/java/server/WsServer.java
@@ -3,18 +3,20 @@ package server;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Oleg O. Plotnikov
  * Date: 10/10/17
  * Copyright 2017 Connective Games LLC. All rights reserved.
  */
-@ServerEndpoint(value = "/ws", encoders = {PackageEncoder.class}, decoders = {PackageDecoder.class})
+@ServerEndpoint(value = "/ws", encoders = {MessageEncoder.class}, decoders = {MessageDecoder.class})
 public class WsServer {
 //GameLogic для каждых двух сессий (два человека-оппонента)
     GameLogic logic = new GameLogic();
-
+    HashMap<Integer, MySession> sessions = new HashMap<>();
+    Hub currentHub = new Hub();
 
     @OnOpen
     public void onOpen(Session session){
@@ -27,22 +29,42 @@ public class WsServer {
     }
 
     @OnMessage
-    public String onMessage(AbstractPackage pack, Session session){
+    public AbstractMessage onMessage(AbstractMessage pack, Session session){
+        MySession thisSession = null;
+        if(!sessions.containsKey(session.hashCode())){
+            MySession mySession;
+            if(currentHub.getIter()<2){
+                thisSession = new MySession(currentHub, session, 1); //created
+            }else{
+                currentHub = new Hub();
+                thisSession = new MySession(currentHub, session, 0); //created
+            }
+            currentHub.setSession(thisSession);
+            sessions.put(session.hashCode(), thisSession);
+        }else{
+            for (Map.Entry<Integer, MySession> entry: sessions.entrySet()) {
+                if(entry.getKey()==session.hashCode()){
+                    thisSession = entry.getValue(); //cought
+                    break;
+                }
+            }
+        }
+
+        Hub hub = thisSession.getHub();
+        if(hub.getIter()==2){
+            MySession secondMySession = hub.getSecondSessions(thisSession.getNumber());
+            Session secondSession = secondMySession.getSession();
+        }
+
+        AbstractMessage message = null;
         //получить GameLogic относящийся к данной сессии
-        pack.apply(logic);
+        message     = pack.apply(logic);
+   //     message = new GameState();
+        message.getValues(logic);
+       // session.getBasicRemote()
         //отправить изменения пользоватеЛЯМ
 
-        try {
-            ((EnterPackage)pack).Name = "Alex";
-            session.getAsyncRemote().sendText("Alex is coming"); //отправления сообщения
-            session.getBasicRemote().sendObject(pack); //отправление объекта
-            //session.getAsyncRemote().sendText ("Alex has came") нельзя!!!!, но можно отправить еще объекты
-        } catch (IOException | EncodeException e) {
-            e.printStackTrace();
-        }
-        String echoMsg = "Alex has came";
-//TODO test return package?????
-        return echoMsg;
+        return message;
     }
 
     @OnError
