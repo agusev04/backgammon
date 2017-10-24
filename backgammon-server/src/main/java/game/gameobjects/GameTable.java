192a39530740dc4a05ea1d07dcd375a7f422ff82
Michael <migelie@mail.ru>
2017-10-24 22:41:20 +0700

    Backgammon-36
    Реализован класс для игрока; Расширена игровая доска - введены технологические ячейки.

--- backgammon-server/src/main/java/game/gameobjects/GameTable.java
+++ backgammon-server/src/main/java/game/gameobjects/GameTable.java
@@ -5,16 +5,16 @@ import game.logics.ChipsPositions;
 public class GameTable {
     public Cell[] cells;
 
-    public GameTable() {
-        cells = new Cell[24];
-        cells[0].setCell('W', 2);
-        cells[5].setCell('B', 5);
-        cells[7].setCell('B', 3);
-        cells[11].setCell('W', 5);
-        cells[12].setCell('B', 5);
-        cells[16].setCell('W', 3);
-        cells[18].setCell('W', 5);
-        cells[23].setCell('B', 2);
+    public GameTable() {  // 0 и 25 ячейки технологические, в них будут храниться фишки которые вышли с поля.
+        cells = new Cell[26];
+        cells[1].setCell('W', 2);
+        cells[6].setCell('B', 5);
+        cells[8].setCell('B', 3);
+        cells[12].setCell('W', 5);
+        cells[13].setCell('B', 5);
+        cells[17].setCell('W', 3);
+        cells[19].setCell('W', 5);
+        cells[24].setCell('B', 2);
     }
     public ChipsPositions getGameState(){
         return new ChipsPositions(cells);
--- backgammon-server/src/main/java/game/logics/Game.java
+++ backgammon-server/src/main/java/game/logics/Game.java
@@ -8,23 +8,14 @@ public class Game {
 //    TODO: геттер для терна (чей ход) (бул тип) 1 - ход, 0 - ожидание своего хода.
 //    у каждого должен быть свой флаг ход\ожидание
 
-    Player player1 = new Player("first", 'w');
-    Player player2 = new Player("second", 'b');
-    //по умолчанию первый игрок белый, второй игрок черный
-    //вместо first и second должна быть переменная, в которую пользователь записывает свое имя при входе
-    //придумать как эту переменную сюда привязать
-
-
-
-    public  void setPlayer1(Player player1){
-        this.player1 = player1;
-    }
-
-    public  void setPlayer2(Player player2){
-        this.player2 = player2;
-    }
 
     public GameTable getTable() {
         return table;
+    }
+
+    public int rollDice(){  //функция бросающая кубик
+        int resalt = 0;
+        resalt =  1 + (int)(Math.random() * ((6- 1)+1));
+        return resalt;
     }
 }
