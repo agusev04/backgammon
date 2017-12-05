7f8880218f98859c8b2ae1f741e857c916e770da
IvchenkoAlexandr <aleksandr.ivchenko19@gmail.com>
2017-12-05 13:21:42 +0700

    Тестировние логов

--- backgammon-server/src/main/java/server/Launcher.java
+++ backgammon-server/src/main/java/server/Launcher.java
@@ -1,6 +1,7 @@
 package server;
 
 import org.apache.catalina.startup.Tomcat;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 
@@ -10,11 +11,16 @@ import java.io.File;
  * Copyright 2017 Connective Games LLC. All rights reserved.
  */
 public class Launcher {
-
     public static void main(String[] args) throws Exception {
-        System.out.println("Launcher: Server started");
-        String webappDirLocation = "web-test";
 
+        System.setProperty("log4j.configuration", new File(".", File.separatorChar+"log4j.properties").toURL().toString());
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        Logger loger = Logger.getLogger(Launcher.class);
+        System.out.println("************************"+contextClassLoader.getResource("."));
+        System.out.println("Launcher: Server started");
+        loger.debug("йух");
+        loger.fatal("где файл");
+        String webappDirLocation = "web-test";
         Tomcat tomcat = new Tomcat();
         tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
         tomcat.setPort(8888);
